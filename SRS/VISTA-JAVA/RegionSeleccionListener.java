f093ce42a9e448fdfdc14e8ef36e5d38465fdb35
koto0000 <72188310@continental.edu.pe>
2025-08-31 09:24:52 -0500

    Delete SRS/VISTA-JAVA/RegionSeleccionListener.java

--- SRS/VISTA-JAVA/RegionSeleccionListener.java
+++
@@ -1,7 +0,0 @@
-package Vista;
-
-public interface RegionSeleccionListener {
-    void onRegionSeleccionada(String nombreRegion);
-        void onLugarBuscado(String nombreLugar); // ← nueva función
-
-}
